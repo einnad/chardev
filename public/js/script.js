f5a9c26a6f4bf4f1fa6a0ef7ae3a5ef72b23d502
einnad <146109295+einnad@users.noreply.github.com>
2024-04-04 19:40:30 +0100

    Complete light/dark theme switch state

--- public/js/script.js
+++ public/js/script.js
@@ -9,14 +9,57 @@ colorSelect.forEach((sel) => {
     let color = sel.getAttribute("data-color");
 
     // SWITCH STATEMENT?
-    if (color === "dark") {
-      textareas.forEach((t) => {
-        t.style.backgroundColor = "#575757";
-        t.style.color = "#ffffff";
-      });
+
+    switch (color) {
+      case "light":
+        changeTextarea("#ffffff", "#000000");
+        changeInput("#ffffff", "#000000");
+        changeButton("#ffffff", "#000000");
+        break;
+      case "dark":
+        changeTextarea("#575757", "#ffffff");
+        changeInput("#575757", "#ffffff");
+        changeButton("#575757", "#ffffff");
+        break;
+
+      case "blue":
+        break;
+
+      case "purple":
+        break;
+
+      case "green":
+        break;
+
+      case "pink":
+        break;
+
+      default:
+        break;
     }
 
     body.classList.remove(...body.classList);
     body.classList.toggle(`${color}`);
   });
 });
+
+const changeTextarea = function (color1, color2) {
+  textareas.forEach((t) => {
+    t.style.backgroundColor = color1;
+    t.style.color = color2;
+  });
+};
+
+const changeInput = function (color1, color2) {
+  inputs.forEach((i) => {
+    i.style.backgroundColor = color1;
+    i.style.color = color2;
+  });
+};
+
+const changeButton = function (color1, color2) {
+  buttons.forEach((b) => {
+    b.style.backgroundColor = color1;
+    b.style.color = color2;
+  });
+};
