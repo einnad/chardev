c6732271c1e5cbcd02e38b178e3cf2035f685334
einnad <146109295+einnad@users.noreply.github.com>
2024-04-04 23:38:08 +0100

    Complete themes and localstore theme

--- public/js/script.js
+++ public/js/script.js
@@ -4,44 +4,74 @@ const textareas = document.querySelectorAll("textarea");
 const inputs = document.querySelectorAll("input");
 const colorSelect = document.querySelectorAll(".color-select");
 
+const textColor = "#000000";
+
+window.onload = () => {
+  const theme = localStorage.getItem("theme");
+  changeTheme(theme);
+};
+
 colorSelect.forEach((sel) => {
+  console.log(sel);
   sel.addEventListener("click", () => {
     let color = sel.getAttribute("data-color");
 
     // SWITCH STATEMENT?
-
-    switch (color) {
-      case "light":
-        changeTextarea("#ffffff", "#000000");
-        changeInput("#ffffff", "#000000");
-        changeButton("#ffffff", "#000000");
-        break;
-      case "dark":
-        changeTextarea("#575757", "#ffffff");
-        changeInput("#575757", "#ffffff");
-        changeButton("#575757", "#ffffff");
-        break;
-
-      case "blue":
-        break;
-
-      case "purple":
-        break;
-
-      case "green":
-        break;
-
-      case "pink":
-        break;
-
-      default:
-        break;
-    }
-
-    body.classList.remove(...body.classList);
-    body.classList.toggle(`${color}`);
+    changeTheme(color);
+    localThemeChange(sel.id);
   });
 });
+
+const localThemeChange = function (theme) {
+  localStorage.setItem("theme", theme);
+};
+
+const getTheme = function () {
+  return localStorage.getItem("theme");
+};
+
+const changeTheme = function (theme) {
+  switch (theme) {
+    case "light":
+      changeTextarea("#ffffff", textColor);
+      changeInput("#ffffff", textColor);
+      changeButton("#ffffff", textColor);
+      changeBody("light");
+      break;
+    case "dark":
+      changeTextarea("#575757", "#ffffff");
+      changeInput("#575757", "#ffffff");
+      changeButton("#575757", "#ffffff");
+      changeBody("dark");
+      break;
+    case "blue":
+      changeTextarea("#C3E1E7", textColor);
+      changeInput("#C3E1E7", textColor);
+      changeButton("#48A1B5", textColor);
+      changeBody("blue");
+      break;
+    case "purple":
+      changeTextarea("#E0C4FB", textColor);
+      changeInput("#E0C4FB", textColor);
+      changeButton("#A75BF5", textColor);
+      changeBody("purple");
+      break;
+    case "green":
+      changeTextarea("#C0E4BF", textColor);
+      changeInput("#C0E4BF", textColor);
+      changeButton("#69BF66", textColor);
+      changeBody("green");
+      break;
+    case "pink":
+      changeTextarea("#F8DEEF", textColor);
+      changeInput("#F8DEEF", textColor);
+      changeButton("#E68EC7", textColor);
+      changeBody("pink");
+      break;
+    default:
+      break;
+  }
+};
 
 const changeTextarea = function (color1, color2) {
   textareas.forEach((t) => {
@@ -62,4 +92,9 @@ const changeButton = function (color1, color2) {
     b.style.backgroundColor = color1;
     b.style.color = color2;
   });
+};
+
+const changeBody = function (className) {
+  body.classList.remove(...body.classList);
+  body.classList.add(`${className}`);
 };
